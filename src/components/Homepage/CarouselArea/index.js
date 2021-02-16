831ec3583769d2b02df409f01c9c5f15aa977452
Renata <rmkarato@gmail.com>
2021-02-16 13:30:00 -0300

    put informations on blank spaces

--- src/components/Homepage/CarouselArea/index.js
+++ src/components/Homepage/CarouselArea/index.js
@@ -1,13 +1,33 @@
 import React from "react";
 
+import FbButton from "../../../assets/images/fb-follow-button.png";
+import SummerSalesBanner from "../../../assets/images/summer-sales-img.png";
+import CarouselBanner1 from "../../../assets/images/carousel-banner-1.png";
+
 import {
-  Container
+  Container,
+  LeftWrapper,
+  TextWrapper,
+  RightWrapper
 } from "./styled";
 
 const CarouselArea = () => {
   return (
     <Container>
-      CarouselArea
+      <LeftWrapper>
+        <img src={CarouselBanner1} alt="Banner 1" />
+        <TextWrapper>
+          <p>Control and manage any device with cloud solutions</p>
+          <p>Improve business performance and the user experience with the right mix of IoT technology and processes</p>
+          <button>VIEW MORE</button>
+        </TextWrapper>
+      </LeftWrapper>
+      <RightWrapper>
+        <img src={SummerSalesBanner} alt="Summer Sales Banner" />
+        <p>Follow us on Facebook</p>
+        <p>Sed ut perspiciatus unde omnis iste natus erros sit</p>
+        <img src={FbButton} alt="Facebook Follow Button" />
+      </RightWrapper>
     </Container>
   );
 };
--- src/components/Homepage/CarouselArea/styled.js
+++ src/components/Homepage/CarouselArea/styled.js
@@ -8,3 +8,33 @@ export const Container = styled.div`
     justify-content: center;
     align-items: center;
 `;
+
+export const LeftWrapper = styled.div`
+    width: 870px;
+    padding-right: 15px;
+    position: relative;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const TextWrapper = styled.div`
+    position: absolute;
+    left: 60px;
+    
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+`;
+
+export const RightWrapper = styled.div`
+    width: 270px;
+    padding-left: 15px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
